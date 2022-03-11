351817864da8f9a7b81d8e8bd9b567d98bb91226
Dev <devp2101@gmail.com>
2022-03-11 12:28:49 +0530

    NEW CODE IS ADDED IN THIS FILE

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,12 +12,50 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+//        'css/site.css',
+        'css/vendor/bootstrap.min.css',
+        'css/vendor/font-awesome.css',
+        'css/vendor/dlicon.css',
+        'css/plugins/owl-carousel.css',
+        'css/plugins/slick.css',
+        'css/plugins/magnific-popup.css',
+        'css/plugins/jarallax.css',
+        'css/plugins/easyzoom.css',
+        'css/plugins/animate.css',
+        'css/plugins/fullpage.css',
+        'css/plugins/jquery-ui.css',
+        'css/plugins/select2.min.css',
+        'css/style.css',
     ];
     public $js = [
+        'js/vendor/modernizr-3.11.7.min.js',
+        'js/vendor/jquery-v3.6.0.min.js',
+        'js/vendor/popper.js',
+        'js/vendor/bootstrap.min.js',
+        'js/plugins/owl-carousel.js',
+        'js/plugins/slick.js',
+        'js/plugins/jarallax.min.js',
+        'js/plugins/countdown.js',
+        'js/plugins/magnific-popup.js',
+        'js/plugins/instafeed.js',
+        'js/plugins/tilt.js',
+        'js/plugins/images-loaded.js',
+        'js/plugins/isotope.js',
+        'js/plugins/scrolloverflow-fullpage.js',
+        'js/plugins/fullpage.js',
+        'js/plugins/jquery-ui.js',
+        'js/plugins/jquery-ui-touch-punch.js',
+        'js/plugins/easyzoom.js',
+        'js/plugins/resizesensor.js',
+        'js/plugins/sticky-sidebar.js',
+        'js/plugins/scrollup.js',
+        'js/plugins/select2.min.js',
+        'js/plugins/cookie.js',
+        'js/plugins/bpopup.js',
+        'js/plugins/wow.js',
+        'js/plugins/ajax-mail.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap4\BootstrapAsset',
     ];
 }
