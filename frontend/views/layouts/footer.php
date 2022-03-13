116a7d443e33ee7e3528f10b37d504059a5caaa7
Dev <devp2101@gmail.com>
2022-03-13 23:00:43 +0530

    footer.php data is added in this file

---
+++ frontend/views/layouts/footer.php
@@ -0,0 +1,81 @@
+<?php
+
+use yii\helpers\Html;
+?>
+
+<footer class="footer-area bg-black section-padding-1">
+    <div class="container-fluid">
+        <div class="footer-top pt-100 pb-35">
+            <div class="row">
+                <div class="footer-column footer-width-26 footer-logo-center">
+                    <div class="footer-widget footer-logo mb-30">
+                        <a href="index.html">
+                            <img src="assets/images/logo/logo-white.png" alt="logo">
+                        </a>
+                    </div>
+                </div>
+                <div class="footer-column footer-width-15">
+                    <div class="footer-widget footer-info-list mb-30">
+                        <h3 class="footer-title">Company Link</h3>
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Help Center</a></li>
+                            <li><a href="#">Our Team</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">The Blog</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer-column footer-width-17">
+                    <div class="footer-widget footer-info-list mb-30">
+                        <h3 class="footer-title">Userful</h3>
+                        <ul>
+                            <li><a href="#">The Collections</a></li>
+                            <li><a href="#">Size Guide</a></li>
+                            <li><a href="#">Return Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer-column footer-width-15 footer-width-15-res">
+                    <div class="footer-widget footer-info-list mb-30">
+                        <h3 class="footer-title">Quick links</h3>
+                        <ul>
+                            <li><a href="#">Compare</a></li>
+                            <li><a href="#">My Wishlist</a></li>
+                            <li><a href="#">My Account</a></li>
+                            <li><a href="#">Orders Tracking</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer-column footer-width-25 mb-30">
+                    <div class="instagram-feed-thumb">
+                        <div id="instafeed" data-limit="6" class="instagram-wrap-2"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-botoom pb-15">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="social-icon-style-2 social-icon-center-res">
+                        <a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
+                        <a class="twitter" href="#"><i class="fa fa-twitter"></i></a>
+                        <a class="youtube" href="#"><i class="fa fa-youtube"></i></a>
+                        <a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="copyright res-mrg-xs text-center">
+                        <p>© 2021 <a href="https://hasthemes.com/">Toro.</a> All rights reserved</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="footer-payment">
+                        <a href="#"><img src="assets/images/icon-img/paypal.png" alt="payment"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
