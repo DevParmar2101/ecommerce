0156216e4f6aede897ee879e7cad96df417dfa27
Dev <devp2101@gmail.com>
2022-03-14 17:31:25 +0530

    _popular_post.php file is generated in this file

---
+++ frontend/views/blog/partial/_popular_post.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\helpers\Html;
+?>
+
+<div class="single-sidebar-post">
+    <div class="sidebar-post-img">
+        <a href="#">
+            <?= Html::img('@web/images/blog/blog-sidebar-1.jpg')?>
+        </a>
+    </div>
+    <div class="sidebar-post-content">
+        <h4><a href="#">Get Ready Fast For Fall Leaf</a></h4>
+        <span>December 18, 2018</span>
+    </div>
+</div>
+
