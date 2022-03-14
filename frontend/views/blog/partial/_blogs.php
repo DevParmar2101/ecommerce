07b914fdb616c7b633c96fbdb754306589e31bd8
Dev <devp2101@gmail.com>
2022-03-14 17:21:04 +0530

    _blogs.php file is generated

---
+++ frontend/views/blog/partial/_blogs.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\helpers\Html;
+?>
+<div class="blog-wrap-3 mb-70">
+    <div class="blog-img-3 mb-20">
+        <a href="#">
+            <?= Html::img('@web/images/blog/blog-8.jpg')?>
+        </a>
+    </div>
+    <div class="blog-content-3">
+        <div class="blog-category">
+            <a href="#">Technology</a>
+        </div>
+        <h3><a href="#">Get Ready Fast For Fall Leaf</a></h3>
+        <div class="blog-meta">
+            <ul>
+                <li><a href="#">Posted on December 5, 2018</a></li>
+                <li>By <a href="#">Joe Doe</a></li>
+            </ul>
+        </div>
+        <p>Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and
+        </p>
+        <div class="blog-btn-2 blog-btn-2-red">
+            <a href="#">Read More</a>
+        </div>
+    </div>
+</div>
+
+
---
+++ frontend/views/blog/partial/_breadcrumb.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+?>
+<div class="breadcrumb-area section-padding-1 bg-gray breadcrumb-ptb-2">
+    <div class="container-fluid">
+        <div class="breadcrumb-content text-center">
+            <div class="breadcrumb-title">
+                <h2>Blog</h2>
+            </div>
+            <ul>
+                <li>
+                    <a href="index.html">Home 01 </a>
+                </li>
+                <li><span> > </span></li>
+                <li class="active"> Blog </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
