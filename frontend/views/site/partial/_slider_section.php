f4f18cef852a8c28dfe27a4b4f89bd16fe1e5feb
Dev <devp2101@gmail.com>
2022-03-14 18:24:02 +0530

    title is added in this file

--- frontend/views/site/partial/_slider_section.php
+++ frontend/views/site/partial/_slider_section.php
@@ -8,61 +8,91 @@ use yii\helpers\Html;
         <div class="categories-slider-active-2 owl-carousel">
             <div class="single-categories-2 text-center">
                 <div class="categories-img">
-                    <a href="#"><img src="assets/images/categories/categories-4.jpg" alt="categories"></a>
+                    <a href="#">
+                        <?= Html::img('@web/images/categories/categories-4.jpg')?>
+<!--                        <img src="assets/images/categories/categories-4.jpg" alt="categories">-->
+                    </a>
                 </div>
                 <span>Headphone</span>
             </div>
             <div class="single-categories-2 text-center">
                 <div class="categories-img">
-                    <a href="#"><img src="assets/images/categories/categories-5.jpg" alt="categories"></a>
+                    <a href="#">
+                        <?= Html::img('@web/images/categories/categories-5.jpg')?>
+<!--                        <img src="assets/images/categories/categories-5.jpg" alt="categories">-->
+                    </a>
                 </div>
                 <span>IMac</span>
             </div>
             <div class="single-categories-2 text-center">
                 <div class="categories-img">
-                    <a href="#"><img src="assets/images/categories/categories-6.jpg" alt="categories"></a>
+                    <a href="#">
+                        <?= Html::img('@web/images/categories/categories-6.jpg')?>
+<!--                        <img src="assets/images/categories/categories-6.jpg" alt="categories">-->
+                    </a>
                 </div>
                 <span>Ipad Pro</span>
             </div>
             <div class="single-categories-2 text-center">
                 <div class="categories-img">
-                    <a href="#"><img src="assets/images/categories/categories-7.jpg" alt="categories"></a>
+                    <a href="#">
+                        <?= Html::img('@web/images/categories/categories-7.jpg')?>
+<!--                        <img src="assets/images/categories/categories-7.jpg" alt="categories">-->
+                    </a>
                 </div>
                 <span>Storage Device</span>
             </div>
             <div class="single-categories-2 text-center">
                 <div class="categories-img">
-                    <a href="#"><img src="assets/images/categories/categories-8.jpg" alt="categories"></a>
+                    <a href="#">
+                        <?= Html::img('@web/images/categories/categories-8.jpg')?>
+<!--                        <img src="assets/images/categories/categories-8.jpg" alt="categories">-->
+                    </a>
                 </div>
                 <span>Apple Laptop</span>
             </div>
             <div class="single-categories-2 text-center">
                 <div class="categories-img">
-                    <a href="#"><img src="assets/images/categories/categories-9.jpg" alt="categories"></a>
+                    <a href="#">
+                        <?= Html::img('@web/images/categories/categories-9.jpg')?>
+<!--                        <img src="assets/images/categories/categories-9.jpg" alt="categories">-->
+                    </a>
                 </div>
                 <span>Home Audio</span>
             </div>
             <div class="single-categories-2 text-center">
                 <div class="categories-img">
-                    <a href="#"><img src="assets/images/categories/categories-10.jpg" alt="categories"></a>
+                    <a href="#">
+                        <?= Html::img('@web/images/categories/categories-10.jpg')?>
+<!--                        <img src="assets/images/categories/categories-10.jpg" alt="categories">-->
+                    </a>
                 </div>
                 <span>Camera & Photo</span>
             </div>
             <div class="single-categories-2 text-center">
                 <div class="categories-img">
-                    <a href="#"><img src="assets/images/categories/categories-11.jpg" alt="categories"></a>
+                    <a href="#">
+                        <?= Html::img('@web/images/categories/categories-11.jpg')?>
+<!--                        <img src="assets/images/categories/categories-11.jpg" alt="categories">-->
+                    </a>
                 </div>
                 <span>Accessories</span>
             </div>
             <div class="single-categories-2 text-center">
                 <div class="categories-img">
-                    <a href="#"><img src="assets/images/categories/categories-7.jpg" alt="categories"></a>
+                    <a href="#">
+                        <?= Html::img('@web/images/categories/categories-7.jpg')?>
+<!--                        <img src="assets/images/categories/categories-7.jpg" alt="categories">-->
+                    </a>
                 </div>
                 <span>Storage Device</span>
             </div>
             <div class="single-categories-2 text-center">
                 <div class="categories-img">
-                    <a href="#"><img src="assets/images/categories/categories-8.jpg" alt="categories"></a>
+                    <a href="#">
+                        <?= Html::img('@web/images/categories/categories-8.jpg')?>
+<!--                        <img src="assets/images/categories/categories-8.jpg" alt="categories">-->
+                    </a>
                 </div>
                 <span>Apple Laptop</span>
             </div>
