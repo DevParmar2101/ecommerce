a5db322239264f92da9730ca4d070b63ea6c41d6
Dev <devp2101@gmail.com>
2022-03-14 17:34:22 +0530

    _popular_post.php file is generated in this file

---
+++ frontend/views/blog/partial/_blog_sidebar.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\helpers\Html;
+?>
+
+<div class="sidebar-widget mt-60 mb-55">
+    <h4 class="pro-sidebar-title2">Categories </h4>
+    <div class="sidebar-widget-list mt-40">
+        <ul>
+            <li><a href="#">Cafe</a> <span>(3)</span></li>
+            <li><a href="#">Finance</a> <span>(2)</span></li>
+            <li><a href="#">Life Style</a> <span>(6)</span></li>
+            <li><a href="#">Photography</a> <span>(2)</span></li>
+            <li><a href="#">Story love</a> <span>(2)</span></li>
+            <li><a href="#">Technology</a> <span>(16)</span></li>
+            <li><a href="#">Trends</a> <span>(1)</span></li>
+        </ul>
+    </div>
+</div>
