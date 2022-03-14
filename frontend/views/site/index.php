c70d95ae3935975015f8c096e4fff3bb82df32d0
Dev <devp2101@gmail.com>
2022-03-14 18:20:50 +0530

    title is added in this file

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -6,6 +6,8 @@ $this->title = 'My Yii Application';
 ?>
     <?= $this->render('partial/_slider_area')?>
 
+    <?= $this->render('partial/_slider_section')?>
+
     <?= $this->render('partial/_car_area')?>
 
     <?= $this->render('partial/_latest_post')?>
---
+++ frontend/views/site/partial/_slider_section.php
@@ -0,0 +1,72 @@
+<?php
+
+use yii\helpers\Html;
+?>
+
+<div class="categories-area section-padding-6 pt-100 pb-100">
+    <div class="container-fluid">
+        <div class="categories-slider-active-2 owl-carousel">
+            <div class="single-categories-2 text-center">
+                <div class="categories-img">
+                    <a href="#"><img src="assets/images/categories/categories-4.jpg" alt="categories"></a>
+                </div>
+                <span>Headphone</span>
+            </div>
+            <div class="single-categories-2 text-center">
+                <div class="categories-img">
+                    <a href="#"><img src="assets/images/categories/categories-5.jpg" alt="categories"></a>
+                </div>
+                <span>IMac</span>
+            </div>
+            <div class="single-categories-2 text-center">
+                <div class="categories-img">
+                    <a href="#"><img src="assets/images/categories/categories-6.jpg" alt="categories"></a>
+                </div>
+                <span>Ipad Pro</span>
+            </div>
+            <div class="single-categories-2 text-center">
+                <div class="categories-img">
+                    <a href="#"><img src="assets/images/categories/categories-7.jpg" alt="categories"></a>
+                </div>
+                <span>Storage Device</span>
+            </div>
+            <div class="single-categories-2 text-center">
+                <div class="categories-img">
+                    <a href="#"><img src="assets/images/categories/categories-8.jpg" alt="categories"></a>
+                </div>
+                <span>Apple Laptop</span>
+            </div>
+            <div class="single-categories-2 text-center">
+                <div class="categories-img">
+                    <a href="#"><img src="assets/images/categories/categories-9.jpg" alt="categories"></a>
+                </div>
+                <span>Home Audio</span>
+            </div>
+            <div class="single-categories-2 text-center">
+                <div class="categories-img">
+                    <a href="#"><img src="assets/images/categories/categories-10.jpg" alt="categories"></a>
+                </div>
+                <span>Camera & Photo</span>
+            </div>
+            <div class="single-categories-2 text-center">
+                <div class="categories-img">
+                    <a href="#"><img src="assets/images/categories/categories-11.jpg" alt="categories"></a>
+                </div>
+                <span>Accessories</span>
+            </div>
+            <div class="single-categories-2 text-center">
+                <div class="categories-img">
+                    <a href="#"><img src="assets/images/categories/categories-7.jpg" alt="categories"></a>
+                </div>
+                <span>Storage Device</span>
+            </div>
+            <div class="single-categories-2 text-center">
+                <div class="categories-img">
+                    <a href="#"><img src="assets/images/categories/categories-8.jpg" alt="categories"></a>
+                </div>
+                <span>Apple Laptop</span>
+            </div>
+        </div>
+    </div>
+</div>
+
