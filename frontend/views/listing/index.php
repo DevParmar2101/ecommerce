787f68d196dd3094eb27aefbef9e05b69118bb52
Dev <devp2101@gmail.com>
2022-03-14 19:35:54 +0530

    _sidebar_categories.php file is generated in this listing/partial folder

--- frontend/views/listing/index.php
+++ frontend/views/listing/index.php
@@ -1,6 +1,8 @@
 <?php
 
 use yii\helpers\Html;
+
+$this->title = 'Listing Page';
 ?>
 
 <div class="breadcrumb-area section-padding-1 breadcrumb-bg-1 breadcrumb-ptb-2">
@@ -26,20 +28,7 @@ use yii\helpers\Html;
             <div class="col-lg-9">
                 <div class="shhop-pl-35">
                     <div class="shop-top-bar">
-                        <div class="shop-top-bar-left">
-                            <div class="shop-top-show">
-                                <span>Showing 1–12 of 39 results</span>
-                            </div>
-                            <div class="shop-page-list">
-                                <ul>
-                                    <li>Show</li>
-                                    <li class="active"><a href="#">12</a></li>
-                                    <li><a href="#">15</a></li>
-                                    <li><a href="#">30</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="shop-top-bar-right">
+                        <div class="shop-top-bar-right text-left">
                             <div class="shop-filter ml-50">
                                 <a class="shop-filter-active" href="#">Filters <i class="fa fa-angle-down angle-down"></i> <i class="fa fa-angle-up angle-up"></i></a>
                             </div>
@@ -123,94 +112,7 @@ use yii\helpers\Html;
                     <?= $this->render('partial/content')?>
                 </div>
             </div>
-            <div class="col-lg-3">
-                <div class="shop-sidebar-style mt-25">
-                    <div class="sidebar-widget mb-65">
-                        <h4 class="pro-sidebar-title">Categories </h4>
-                        <div class="sidebar-widget-list mt-50">
-                            <ul>
-                                <li><a href="#">Electronics & Tech</a> <span>(16)</span></li>
-                                <li><a href="#">Electronics sale</a> <span>(13)</span></li>
-                                <li><a href="#">Electronics sale off</a> <span>(39)</span></li>
-                                <li><a href="#">Fashion sale off</a> <span>(16)</span></li>
-                                <li><a href="#">Fashions</a> <span>(15)</span></li>
-                                <li><a href="#">Furnitur new arrival</a> <span>(18)</span></li>
-                                <li><a href="#">Furniture</a> <span>(10)</span></li>
-                                <li><a href="#">Main</a> <span>(19)</span></li>
-                                <li><a href="#">Shoes</a> <span>(12)</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="sidebar-widget mb-70">
-                        <h4 class="pro-sidebar-title">Filter by price </h4>
-                        <div class="price-filter mt-60">
-                            <div id="slider-range"></div>
-                            <div class="price-slider-amount">
-                                <div class="label-input">
-                                    <span>Price: </span><input type="text" id="amount" name="price" placeholder="Add Your Price" />
-                                </div>
-                                <button type="button">Filter</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="sidebar-widget mb-60">
-                        <h4 class="pro-sidebar-title">Size</h4>
-                        <div class="sidebar-widget-size mt-45">
-                            <ul>
-                                <li><a href="#">Full Size</a></li>
-                                <li><a href="#">L</a></li>
-                                <li><a href="#">M</a></li>
-                                <li><a href="#">S</a></li>
-                                <li><a href="#">XL</a></li>
-                                <li><a href="#">XXL</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="sidebar-widget mb-45">
-                        <h4 class="pro-sidebar-title">Color</h4>
-                        <div class="sidebar-widget-color mt-40">
-                            <ul>
-                                <li><a href="#">Blue <span>(8)</span></a></li>
-                                <li><a href="#">Brown <span>(8)</span></a></li>
-                                <li><a href="#">Green <span>(7)</span></a></li>
-                                <li><a href="#">Pink <span>(7)</span></a></li>
-                                <li><a href="#">Violet <span>(8)</span></a></li>
-                                <li><a href="#">White <span>(8)</span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="sidebar-widget mb-60">
-                        <h4 class="pro-sidebar-title">Tags</h4>
-                        <div class="sidebar-widget-tag mt-45">
-                            <ul>
-                                <li><a href="#">Blazer,</a></li>
-                                <li><a href="#">chair,</a></li>
-                                <li><a href="#">Coat,</a></li>
-                                <li><a href="#">dress,</a></li>
-                                <li><a href="#">Electronics & Tech,</a></li>
-                                <li><a href="#">fashion,</a></li>
-                                <li><a href="#">light,</a></li>
-                                <li><a href="#">Living,</a></li>
-                                <li><a href="#">Main 01,</a></li>
-                                <li><a href="#">Main 02,</a></li>
-                                <li><a href="#">Main 02-1,</a></li>
-                                <li><a href="#">Main 03,</a></li>
-                                <li><a href="#">Sandals,</a></li>
-                                <li><a href="#">Tuxedo</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="sidebar-widget">
-                        <div class="shop-sidebar-banner default-overlay">
-                            <a href="#"><img alt="" src="assets/images/banner/sidebar-banner.jpg"></a>
-                            <div class="shop-sidebar-content">
-                                <h5>Houndstooth coat</h5>
-                                <h3>off 25%</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?= $this->render('partial/_sidebar_categories')?>
         </div>
     </div>
 </div>
--- frontend/views/listing/partial/content.php
+++ frontend/views/listing/partial/content.php
@@ -14,9 +14,6 @@ use yii\helpers\Html;
                                 <?= Html::img('@web/images/product/product-8.jpg',['class' => 'default-img'])?>
                                 <?= Html::img('@web/images/product/product-8-2.jpg',['class' => 'hover-img'])?>
                             </a>
-                            <div class="shop-list-quickview">
-                                <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -32,7 +29,6 @@ use yii\helpers\Html;
                             <div class="pro-list-actioncart">
                                 <a title="Add to cart" href="#">Add to cart</a>
                             </div>
-                            <a title="Add to Compare" href="#"><i class="fa fa-files-o"></i></a>
                         </div>
                     </div>
                 </div>
@@ -47,9 +43,6 @@ use yii\helpers\Html;
                                 <?= Html::img('@web/images/product/product-1.jpg',['class' => 'default-img'])?>
                                 <?= Html::img('@web/images/product/product-1-2.jpg',['class' => 'hover-img'])?>
                             </a>
-                            <div class="shop-list-quickview">
-                                <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -65,151 +58,11 @@ use yii\helpers\Html;
                             <div class="pro-list-actioncart">
                                 <a title="Add to cart" href="#">Add to cart</a>
                             </div>
-                            <a title="Add to Compare" href="#"><i class="fa fa-files-o"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="shop-list-wrap mb-70">
-            <div class="row">
-                <div class="custom-col-32-shop">
-                    <div class="product-wrap">
-                        <div class="product-img default-overlay">
-                            <a href="#">
-                                <?= Html::img('@web/images/product/product-2.jpg',['class' => 'default-img'])?>
-                                <?= Html::img('@web/images/product/product-2-2.jpg',['class' => 'hover-img'])?>
-                            </a>
-                            <div class="shop-list-quickview">
-                                <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="custom-col-68-shop">
-                    <div class="shop-list-content ml-20">
-                        <h3><a href="#">Sleeve Button Cowl Neck</a></h3>
-                        <div class="pro-list-price">
-                            <span>$50.00</span>
-                        </div>
-                        <p>Composition: 50% cotton,45% polyester, 5% polyamide. Filling: 100% polyester. Hood fur: 64% acrylic,23% modacrylic,13% polyester</p>
-                        <div class="product-list-action">
-                            <a title="Add to Wishlist" href="#"><i class="fa fa-heart"></i></a>
-                            <div class="pro-list-actioncart">
-                                <a title="Add to cart" href="#">Add to cart</a>
-                            </div>
-                            <a title="Add to Compare" href="#"><i class="fa fa-files-o"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="shop-list-wrap mb-70">
-            <div class="row">
-                <div class="custom-col-32-shop">
-                    <div class="product-wrap">
-                        <div class="product-img default-overlay">
-                            <a href="#">
-                                <?= Html::img('@web/images/product/product-3.jpg',['class' => 'default-img'])?>
-                                <?= Html::img('@web/images/product/product-3-2.jpg',['class' => 'hover-img'])?>
-                            </a>
-                            <div class="shop-list-quickview">
-                                <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="custom-col-68-shop">
-                    <div class="shop-list-content ml-20">
-                        <h3><a href="#">Cable – knit sweater</a></h3>
-                        <div class="pro-list-price">
-                            <span>$70.00</span>
-                        </div>
-                        <p>Composition: 50% cotton,45% polyester, 5% polyamide. Filling: 100% polyester. Hood fur: 64% acrylic,23% modacrylic,13% polyester</p>
-                        <div class="product-list-action">
-                            <a title="Add to Wishlist" href="#"><i class="fa fa-heart"></i></a>
-                            <div class="pro-list-actioncart">
-                                <a title="Add to cart" href="#">Add to cart</a>
-                            </div>
-                            <a title="Add to Compare" href="#"><i class="fa fa-files-o"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="shop-list-wrap mb-70">
-            <div class="row">
-                <div class="custom-col-32-shop">
-                    <div class="product-wrap">
-                        <div class="product-img default-overlay">
-                            <a href="#">
-                                <?= Html::img('@web/images/product/product-7.jpg',['class' => 'default-img'])?>
-                                <?= Html::img('@web/images/product/product-7-2.jpg',['class' => 'hover-img'])?>
-                            </a>
-                            <div class="shop-list-quickview">
-                                <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="custom-col-68-shop">
-                    <div class="shop-list-content ml-20">
-                        <h3><a href="#">Cable – knit sweater</a></h3>
-                        <div class="pro-list-price">
-                            <span>$70.00</span>
-                        </div>
-                        <p>Composition: 50% cotton,45% polyester, 5% polyamide. Filling: 100% polyester. Hood fur: 64% acrylic,23% modacrylic,13% polyester</p>
-                        <div class="product-list-action">
-                            <a title="Add to Wishlist" href="#"><i class="fa fa-heart"></i></a>
-                            <div class="pro-list-actioncart">
-                                <a title="Add to cart" href="#">Add to cart</a>
-                            </div>
-                            <a title="Add to Compare" href="#"><i class="fa fa-files-o"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="shop-list-wrap mb-70">
-            <div class="row">
-                <div class="custom-col-32-shop">
-                    <div class="product-wrap">
-                        <div class="product-img default-overlay">
-                            <a href="#">
-                                <?= Html::img('@web/images/product/product-10.jpg',['class' => 'default-img'])?>
-                                <?= Html::img('@web/images/product/product-10-2.jpg',['class' => 'hover-img'])?>
-                            </a>
-                            <div class="shop-list-quickview">
-                                <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="custom-col-68-shop">
-                    <div class="shop-list-content ml-20">
-                        <h3><a href="#">Cable – knit sweater</a></h3>
-                        <div class="pro-list-price">
-                            <span>$90.00</span>
-                        </div>
-                        <p>Composition: 50% cotton,45% polyester, 5% polyamide. Filling: 100% polyester. Hood fur: 64% acrylic,23% modacrylic,13% polyester</p>
-                        <div class="product-list-action">
-                            <a title="Add to Wishlist" href="#"><i class="fa fa-heart"></i></a>
-                            <div class="pro-list-actioncart">
-                                <a title="Add to cart" href="#">Add to cart</a>
-                            </div>
-                            <a title="Add to Compare" href="#"><i class="fa fa-files-o"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="pro-pagination-style text-center">
-        <ul>
-            <li><a class="active" href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#"><i class="dlicon arrows-1_tail-right"></i></a></li>
-        </ul>
     </div>
 </div>
 
