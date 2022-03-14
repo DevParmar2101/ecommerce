60230b7827e895d9e7ee92a8a0787ecf5a329f00
Dev <devp2101@gmail.com>
2022-03-14 17:52:42 +0530

    new file generated and code is also added in this file

---
+++ frontend/views/blog/blog-inner.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\helpers\Html;
+?>
+
+<div class="breadcrumb-area section-padding-1 bg-gray breadcrumb-ptb-2">
+    <div class="container-fluid">
+        <div class="breadcrumb-content text-center">
+            <div class="breadcrumb-title">
+                <h2>Blog</h2>
+            </div>
+            <ul>
+                <li>
+                    <a href="#">Home 01 </a>
+                </li>
+                <li><span> > </span></li>
+                <li class="active"> Blog </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="blog-area-2 pt-100 pb-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="blog-wrap-3 mb-70">
+                    <div class="blog-img-3 mb-20">
+                        <a href="#">
+                            <?= Html::img('@web/images/blog/blog-8.jpg')?>
+<!--                            <img src="assets/images/blog/blog-8.jpg" alt="blog">-->
+                        </a>
+                    </div>
+                    <div class="blog-content-3 ml-150">
+                        <div class="blog-category">
+                            <a href="#">Technology</a>
+                        </div>
+                        <h3><a href="#">Get Ready Fast For Fall Leaf</a></h3>
+                        <div class="blog-meta">
+                            <ul>
+                                <li><a href="#">Posted on December 5, 2018</a></li>
+                                <li>By <a href="#">Joe Doe</a></li>
+                            </ul>
+                        </div>
+                        <p>Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and       </p>
+
+                        <p>Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and </p>
+
+                        <p>Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and Chances are unless you are very lucky you will go thru many different relationships before you find your special someone. Finding your sole mate is like gambling. In poker and
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
