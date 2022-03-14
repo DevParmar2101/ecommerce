c72252855ca7a80b22f033559ef04241f0660e45
Dev <devp2101@gmail.com>
2022-03-14 17:59:14 +0530

    new design is integrated in error.php

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -9,19 +9,27 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="error-area bg-gray-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="error-content text-center">
+                    <h1><?= Html::encode($this->title) ?></h1>
+                    <h3><?= nl2br(Html::encode($message)) ?></h3>
+                    <p>It looks like nothing was found on this search!</p>
+                    <div class="btn-style-1 text-center">
+                        <a class="btn-border-black2" href="<?= Yii::$app->homeUrl?>">
+                            <div class="btn-viewmore-normal btn-ptb-3 btn-viewmore-bg-transparent btn-viewmore-common btn-font-3">
+                                <span>Back to home</span>
+                            </div>
+                            <div class="btn-viewmore-hover btn-ptb-3 btn-viewmore-common btn-hover-transition btn-font-3">
+                                <span>Back to home</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
