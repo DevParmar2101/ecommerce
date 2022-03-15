2519162561aee83d2309ebd15758995c62302552
Dev <devp2101@gmail.com>
2022-03-15 14:44:33 +0530

    new content code is added in this file

--- frontend/views/listing/partial/_listing_detail_content.php
+++ frontend/views/listing/partial/_listing_detail_content.php
@@ -3,4 +3,175 @@
 use yii\helpers\Html;
 ?>
 
+<div class="product-details-area section-padding-1 pb-90">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="product-dec2-48">
+                <div class="product-details-tab">
+                    <div class="pro-dec-big-img-slider">
+                        <div class="easyzoom-style">
+                            <div class="easyzoom easyzoom--overlay">
+                                <a href="assets/images/product-details/b-large-1.jpg">
+                                    <img src="assets/images/product-details/large-1.jpg" alt="">
+                                </a>
+                            </div>
+                            <a class="easyzoom-pop-up img-popup" href="assets/images/product-details/b-large-1.jpg"><i class="dlicon ui-1_zoom-in "></i></a>
+                        </div>
+                        <div class="easyzoom-style">
+                            <div class="easyzoom easyzoom--overlay">
+                                <a href="assets/images/product-details/b-large-2.jpg">
+                                    <img src="assets/images/product-details/large-2.jpg" alt="">
+                                </a>
+                            </div>
+                            <a class="easyzoom-pop-up img-popup" href="assets/images/product-details/b-large-2.jpg"><i class="dlicon ui-1_zoom-in "></i></a>
+                        </div>
+                        <div class="easyzoom-style">
+                            <div class="easyzoom easyzoom--overlay">
+                                <a href="assets/images/product-details/b-large-3.jpg">
+                                    <img src="assets/images/product-details/large-3.jpg" alt="">
+                                </a>
+                            </div>
+                            <a class="easyzoom-pop-up img-popup" href="assets/images/product-details/b-large-3.jpg"><i class="dlicon ui-1_zoom-in "></i></a>
+                        </div>
+                        <div class="easyzoom-style">
+                            <div class="easyzoom easyzoom--overlay">
+                                <a href="assets/images/product-details/b-large-4.jpg">
+                                    <img src="assets/images/product-details/large-4.jpg" alt="">
+                                </a>
+                            </div>
+                            <a class="easyzoom-pop-up img-popup" href="assets/images/product-details/b-large-4.jpg"><i class="dlicon ui-1_zoom-in "></i></a>
+                        </div>
+                        <div class="easyzoom-style">
+                            <div class="easyzoom easyzoom--overlay">
+                                <a href="assets/images/product-details/b-large-2.jpg">
+                                    <img src="assets/images/product-details/large-2.jpg" alt="">
+                                </a>
+                            </div>
+                            <a class="easyzoom-pop-up img-popup" href="assets/images/product-details/b-large-2.jpg"><i class="dlicon ui-1_zoom-in "></i></a>
+                        </div>
+                    </div>
+                    <div class="product-dec-slider-2 product-dec-small-style2">
+                        <div class="product-dec-small active">
+                            <img src="assets/images/product-details/small-1.jpg" alt="">
+                        </div>
+                        <div class="product-dec-small">
+                            <img src="assets/images/product-details/small-2.jpg" alt="">
+                        </div>
+                        <div class="product-dec-small">
+                            <img src="assets/images/product-details/small-3.jpg" alt="">
+                        </div>
+                        <div class="product-dec-small">
+                            <img src="assets/images/product-details/small-4.jpg" alt="">
+                        </div>
+                        <div class="product-dec-small">
+                            <img src="assets/images/product-details/small-2.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="product-dec2-52">
+                <div class="row">
+                    <div class="product-details-col-72">
+                        <div class="product-details-content p-dec-content-edit">
+                            <div class="pro-dec-next-prev">
+                                <a class="prev" href="#"><i class="dlicon arrows-1_tail-left"></i></a>
+                                <a class="next" href="#"><i class="dlicon arrows-1_tail-right"></i></a>
+                            </div>
+                            <h2 class="uppercase">Quilted parka with hood</h2>
+                            <div class="product-details-ratting-wrap">
+                                <div class="product-details-ratting">
+                                    <i class="yellow fa fa-star"></i>
+                                    <i class="yellow fa fa-star"></i>
+                                    <i class="yellow fa fa-star"></i>
+                                    <i class="yellow fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                </div>
+                                <a href="#"> (1 customer review)</a>
+                            </div>
+                            <h3>$49.00</h3>
+                            <div class="product-details-peragraph">
+                                <ul>
+                                    <li>Composition: 50% cotton,45% polyester, 5% polyamide.</li>
+                                    <li>Filling: 100% polyester.</li>
+                                    <li>Hood fur: 64% acrylic,23% modacrylic,13% polyester</li>
+                                </ul>
+                            </div>
+                            <div class="product-details-action-wrap">
+                                <div class="product-details-quality">
+                                    <div class="cart-plus-minus">
+                                        <input class="cart-plus-minus-box" type="text" name="qtybutton" value="2">
+                                    </div>
+                                </div>
+                                <div class="product-details-cart">
+                                    <a title="Add to cart" href="#">Add to cart</a>
+                                </div>
+                                <div class="product-details-wishlist">
+                                    <a title="Add to wishlist" href="#"><i class="fa fa-heart"></i></a>
+                                </div>
+                                <div class="product-details-compare">
+                                    <a title="Add to compare" href="#"><i class="dlicon arrows-4_compare"></i></a>
+                                </div>
+                            </div>
+                            <div class="product-details-info">
+                                <a href="#"><i class="dlicon location_pin"></i>Store availability</a>
+                                <a href="#"><i class="dlicon envir_car"></i>Delivery and return</a>
+                                <a href="#"><i class="dlicon design_measure-17"></i>Size Guide</a>
+                            </div>
+                            <div class="product-details-meta">
+                                <span>SKU: REF. LA-140</span>
+                                <span>Categories: <a href="#">Fashions</a>, <a href="#">Main 01</a>, <a href="#">Main 02</a></span>
+                                <span>Tags: <a href="#">Blazer</a>, <a href="#">chair</a>, <a href="#">Coat</a>, <a href="#">dress</a>, <a href="#">light</a>, <a href="#">Living</a>, <a href="#">Main 01</a></span>
+                            </div>
+                            <div class="social-icon-style-3">
+                                <a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
+                                <a class="twitter" href="#"><i class="fa fa-twitter"></i></a>
+                                <a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+                                <a class="pinterest" href="#"><i class="fa fa-pinterest-p"></i></a>
+                                <a class="google-plus" href="#"><i class="fa fa-google-plus"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-details-col-28">
+                        <div class="pro-details-sidebar-active slider-nav-style-3 owl-carousel">
+                            <div class="single-pro-details-sidebar">
+                                <a href="#"><img src="assets/images/product-details/pro-details-sidebar-1.jpg" alt=""></a>
+                                <div class="pro-details-sidebar-content">
+                                    <h4><a href="#">NEW <br>LONDON</a></h4>
+                                    <p>Off 20% for all</p>
+                                </div>
+                            </div>
+                            <div class="single-pro-details-sidebar">
+                                <a href="#"><img src="assets/images/product-details/pro-details-sidebar-2.jpg" alt=""></a>
+                                <div class="pro-details-sidebar-content">
+                                    <h4><a href="#">NEW <br>LONDON</a></h4>
+                                    <p>Off 20% for all</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="sidebar-subscribe-wrap">
+                            <div class="sidebar-subscribe-content text-center">
+                                <i class="dlicon ui-1_email-84"></i>
+                                <h5>Join Our Newsletter</h5>
+                                <p>Sale up to 20% off for your next purchase in this month!</p>
+                            </div>
+                            <div id="mc_embed_signup" class="sidebar-subscribe-form">
+                                <form id="mc-embedded-subscribe-form" class="validate subscribe-form-style" novalidate="" target="_blank" name="mc-embedded-subscribe-form" method="post" action="https://devitems.us11.list-manage.com/subscribe/post?u=6bbb9b6f5827bd842d9640c82&amp;id=05d85f18ef">
+                                    <div id="mc_embed_signup_scroll" class="mc-form">
+                                        <input class="email" type="email" required="" placeholder=" email address…" name="EMAIL" value="">
+                                        <div class="mc-news" aria-hidden="true">
+                                            <input type="text" value="" tabindex="-1" name="b_6bbb9b6f5827bd842d9640c82_05d85f18ef">
+                                        </div>
+                                        <div class="clear">
+                                            <input id="mc-embedded-subscribe" class="button" type="submit" name="subscribe" value="Subscribe">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
