883cf409daa2c9374a0bb286d3a1344a2ef2e136
Dev <devp2101@gmail.com>
2022-03-16 15:59:19 +0530

    new _related_product.php file is generated

--- frontend/views/listing/partial/_related_product.php
+++ frontend/views/listing/partial/_related_product.php
@@ -2,3 +2,146 @@
 
 use yii\helpers\Html;
 ?>
+
+<div class="related-product-area section-padding-1 pb-95">
+    <div class="container-fluid">
+        <div class="section-title-6 mb-50">
+            <h2>Related Products</h2>
+        </div>
+        <div class="related-product-active owl-carousel">
+            <div class="product-wrap">
+                <div class="product-img default-overlay mb-25">
+                    <a href="#">
+                        <?= Html::img('@web/images/product/product-3.jpg',['class' => 'default-img'])?>
+                        <?= Html::img('@web/images/product/product-3-2.jpg',['class' => 'hover-img'])?>
+<!--                        <img class="default-img" src="assets/images/product/product-3.jpg" alt="">-->
+<!--                        <img class="hover-img" src="assets/images/product/product-3-2.jpg" alt="">-->
+                    </a>
+                    <div class="product-action product-action-position-1">
+                        <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
+                        <a title="Add to Wishlist" href="#"><i class="fa fa-heart"></i><span>Add to Wishlist</span></a>
+                        <a class="icon-blod" title="Add to Compare" href="#"><i class="dlicon arrows-4_compare"></i><span>Add to Compare</span></a>
+                        <a title="Add to Cart" href="#"><i class="fa fa-shopping-cart"></i><span>Add to Cart</span></a>
+                    </div>
+                </div>
+                <div class="product-content-2 title-font-width-400 text-center">
+                    <h3><a href="#">Fashion sneaker winter</a></h3>
+                    <div class="product-price">
+                        <span class="new-price">$28.00</span>
+                    </div>
+                </div>
+            </div>
+            <div class="product-wrap">
+                <div class="product-img default-overlay mb-25">
+                    <a href="#">
+                        <?= Html::img('@web/images/product/product-4.jpg',['class' => 'default-img'])?>
+                        <?= Html::img('@web/images/product/product-4-2.jpg',['class' => 'hover-img'])?>
+<!--                        <img class="default-img" src="assets/images/product/product-4.jpg" alt="">-->
+<!--                        <img class="hover-img" src="assets/images/product/product-4-2.jpg" alt="">-->
+                    </a>
+                    <div class="product-action product-action-position-1">
+                        <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
+                        <a title="Add to Wishlist" href="#"><i class="fa fa-heart"></i><span>Add to Wishlist</span></a>
+                        <a class="icon-blod" title="Add to Compare" href="#"><i class="dlicon arrows-4_compare"></i><span>Add to Compare</span></a>
+                        <a title="Add to Cart" href="#"><i class="fa fa-shopping-cart"></i><span>Add to Cart</span></a>
+                    </div>
+                </div>
+                <div class="product-content-2 title-font-width-400 text-center">
+                    <h3><a href="#">News strappy sandals</a></h3>
+                    <div class="product-price">
+                        <span class="new-price">$26.00</span>
+                    </div>
+                </div>
+            </div>
+            <div class="product-wrap">
+                <div class="product-img default-overlay mb-25">
+                    <a href="#">
+                        <?= Html::img('@web/images/product/product-1.jpg',['class' => 'default-img'])?>
+                        <?= Html::img('@web/images/product/product-1-2.jpg',['class' => 'hover-img'])?>
+<!--                        <img class="default-img" src="assets/images/product/product-1.jpg" alt="">-->
+<!--                        <img class="hover-img" src="assets/images/product/product-1-2.jpg" alt="">-->
+                    </a>
+                    <div class="product-action product-action-position-1">
+                        <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
+                        <a title="Add to Wishlist" href="#"><i class="fa fa-heart"></i><span>Add to Wishlist</span></a>
+                        <a class="icon-blod" title="Add to Compare" href="#"><i class="dlicon arrows-4_compare"></i><span>Add to Compare</span></a>
+                        <a title="Add to Cart" href="#"><i class="fa fa-shopping-cart"></i><span>Add to Cart</span></a>
+                    </div>
+                </div>
+                <div class="product-content-2 title-font-width-400 text-center">
+                    <h3><a href="#">Check bermuda shorts</a></h3>
+                    <div class="product-price">
+                        <span class="new-price">$39.00</span>
+                    </div>
+                </div>
+            </div>
+            <div class="product-wrap">
+                <div class="product-img default-overlay mb-25">
+                    <a href="#">
+                        <?= Html::img('@web/images/product/product-5.jpg',['class' => 'default-img'])?>
+                        <?= Html::img('@web/images/product/product-5-2.jpg',['class' => 'hover-img'])?>
+<!--                        <img class="default-img" src="assets/images/product/product-5.jpg" alt="">-->
+<!--                        <img class="hover-img" src="assets/images/product/product-5-2.jpg" alt="">-->
+                    </a>
+                    <div class="product-action product-action-position-1">
+                        <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
+                        <a title="Add to Wishlist" href="#"><i class="fa fa-heart"></i><span>Add to Wishlist</span></a>
+                        <a class="icon-blod" title="Add to Compare" href="#"><i class="dlicon arrows-4_compare"></i><span>Add to Compare</span></a>
+                        <a title="Add to Cart" href="#"><i class="fa fa-shopping-cart"></i><span>Add to Cart</span></a>
+                    </div>
+                </div>
+                <div class="product-content-2 title-font-width-400 text-center">
+                    <h3><a href="#">Down puffer coat collar</a></h3>
+                    <div class="product-price">
+                        <span class="new-price">$55.00</span>
+                    </div>
+                </div>
+            </div>
+            <div class="product-wrap">
+                <div class="product-img default-overlay mb-25">
+                    <a href="#">
+                        <?= Html::img('@web/images/product/product-6.jpg',['class' => 'default-img'])?>
+                        <?= Html::img('@web/images/product/product-6-2.jpg',['class' => 'hover-img'])?>
+<!--                        <img class="default-img" src="assets/images/product/product-6.jpg" alt="">-->
+<!--                        <img class="hover-img" src="assets/images/product/product-6-2.jpg" alt="">-->
+                    </a>
+                    <div class="product-action product-action-position-1">
+                        <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
+                        <a title="Add to Wishlist" href="#"><i class="fa fa-heart"></i><span>Add to Wishlist</span></a>
+                        <a class="icon-blod" title="Add to Compare" href="#"><i class="dlicon arrows-4_compare"></i><span>Add to Compare</span></a>
+                        <a title="Add to Cart" href="#"><i class="fa fa-shopping-cart"></i><span>Add to Cart</span></a>
+                    </div>
+                </div>
+                <div class="product-content-2 title-font-width-400 text-center">
+                    <h3><a href="#">Bejewelled velvet sandals</a></h3>
+                    <div class="product-price">
+                        <span class="new-price">$49.00</span>
+                    </div>
+                </div>
+            </div>
+            <div class="product-wrap">
+                <div class="product-img default-overlay mb-25">
+                    <a href="#">
+                        <?= Html::img('@web/images/product/product-4.jpg',['class' => 'default-img'])?>
+                        <?= Html::img('@web/images/product/product-4-2.jpg',['class' => 'hover-img'])?>
+<!--                        <img class="default-img" src="assets/images/product/product-4.jpg" alt="">-->
+<!--                        <img class="hover-img" src="assets/images/product/product-4-2.jpg" alt="">-->
+                    </a>
+                    <div class="product-action product-action-position-1">
+                        <a data-toggle="modal" data-target="#exampleModal" href="#"><i class="fa fa-eye"></i><span>Quick Shop</span></a>
+                        <a title="Add to Wishlist" href="#"><i class="fa fa-heart"></i><span>Add to Wishlist</span></a>
+                        <a class="icon-blod" title="Add to Compare" href="#"><i class="dlicon arrows-4_compare"></i><span>Add to Compare</span></a>
+                        <a title="Add to Cart" href="#"><i class="fa fa-shopping-cart"></i><span>Add to Cart</span></a>
+                    </div>
+                </div>
+                <div class="product-content-2 title-font-width-400 text-center">
+                    <h3><a href="#">News strappy sandals</a></h3>
+                    <div class="product-price">
+                        <span class="new-price">$26.00</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
