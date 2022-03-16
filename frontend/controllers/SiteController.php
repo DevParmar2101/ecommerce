85d9fbad94db4e9e49510cad704ae10cc28f55b8
Dev <devp2101@gmail.com>
2022-03-16 18:56:46 +0530

    new layout is render is this site-controller files

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -153,6 +153,7 @@ class SiteController extends Controller
      */
     public function actionSignup()
     {
+        $this->layout = 'login_layout';
         $model = new SignupForm();
         if ($model->load(Yii::$app->request->post()) && $model->signup()) {
             Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
