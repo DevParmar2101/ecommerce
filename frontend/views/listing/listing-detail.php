556fa63577045bf633f2029b3494c6040205ffb5
Dev <devp2101@gmail.com>
2022-03-16 15:59:21 +0530

    new _related_product.php file is generated

--- frontend/views/listing/listing-detail.php
+++ frontend/views/listing/listing-detail.php
@@ -8,3 +8,4 @@ $this->title = 'Listing Details';
 <?= $this->render('partial/_listing_detail_breadcrumb')?>
 <?= $this->render('partial/_listing_detail_content')?>
 <?= $this->render('partial/_description_review_section')?>
+<?= $this->render('partial/_related_product')?>
