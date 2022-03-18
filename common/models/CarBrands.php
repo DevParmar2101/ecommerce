19575ad026c154df18fd0a33a3a94e3857404b83
Dev <devp2101@gmail.com>
2022-03-18 15:19:43 +0530

    new CarBrands.php model file is generated in ths folder

---
+++ common/models/CarBrands.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "car_brands".
+ *
+ * @property int $id
+ * @property string $brand_name
+ * @property string $brand_slug
+ * @property string $brand_logo
+ * @property int $status
+ * @property string $created_at
+ * @property int $created_by
+ *
+ * @property User $createdBy
+ */
+class CarBrands extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'car_brands';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['brand_name', 'brand_slug', 'brand_logo', 'status', 'created_by'], 'required'],
+            [['status', 'created_by'], 'integer'],
+            [['created_at'], 'safe'],
+            [['brand_name', 'brand_slug', 'brand_logo'], 'string', 'max' => 255],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'brand_name' => 'Brand Name',
+            'brand_slug' => 'Brand Slug',
+            'brand_logo' => 'Brand Logo',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+}
