5d736bf9a74aad55f46efecf66d28d1a8c11ac1c
Dev <devp2101@gmail.com>
2022-03-21 11:31:29 +0530

    design bugs are fixed in this file

--- backend/views/index-content/_form.php
+++ backend/views/index-content/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\ckeditor\CKEditor;
+use kartik\file\FileInput;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\IndexContent */
@@ -12,29 +14,70 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
     <div class="box-body table-responsive">
 
-        <?= $form->field($model, 'banner_title')->textInput(['maxlength' => true]) ?>
+        <div class="row-lg">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <?= $form->field($model, 'banner_title')->textInput(['maxlength' => true]) ?>
+            </div>
+        </div>
 
-        <?= $form->field($model, 'banner_sub_title')->textInput(['maxlength' => true]) ?>
+        <div class="row-lg">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <?= $form->field($model, 'banner_sub_title')->widget(CKEditor::className(),[
+                    'options' => ['rows' => 10],
+                    'preset' => 'basic',
 
-        <?= $form->field($model, 'banner_button_title')->textInput(['maxlength' => true]) ?>
+                ]) ?>
+            </div>
+        </div>
 
-        <?= $form->field($model, 'banner_image')->textInput(['maxlength' => true]) ?>
+        <div class="row-lg">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <?= $form->field($model,'banner_image')->widget(FileInput::class,[
+                    'pluginOptions' => [
+                        'showPreview' => true,
+                        'showCaption' => true,
+                        'showRemove' => false,
+                        'showUpload' => false,
+                    ],
+                    'options' => ['accept' => 'image/*']
+                ])?>
+            </div>
+        </div>
 
-        <?= $form->field($model, 'section_one_title')->textInput(['maxlength' => true]) ?>
+        <div class="row-lg">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <?= $form->field($model, 'banner_button_title')->textInput(['maxlength' => true]) ?>
+            </div>
+        </div>
 
-        <?= $form->field($model, 'section_two_title')->textInput(['maxlength' => true]) ?>
+        <div class="row-lg">
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <?= $form->field($model, 'section_one_title')->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <?= $form->field($model, 'section_two_title')->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <?= $form->field($model, 'section_three_title')->textInput(['maxlength' => true]) ?>
+            </div>
+        </div>
 
-        <?= $form->field($model, 'section_three_title')->textInput(['maxlength' => true]) ?>
-
-        <?= $form->field($model, 'section_content_one_id')->textInput() ?>
-
-        <?= $form->field($model, 'section_content_two_id')->textInput() ?>
-
-        <?= $form->field($model, 'section_content_three_id')->textInput() ?>
+        <div class="row-lg">
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <?= $form->field($model, 'section_content_one_id')->textInput() ?>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <?= $form->field($model, 'section_content_two_id')->textInput() ?>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <?= $form->field($model, 'section_content_three_id')->textInput() ?>
+            </div>
+        </div>
 
     </div>
     <div class="box-footer">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success btn-flat']) ?>
     </div>
+
     <?php ActiveForm::end(); ?>
 </div>
