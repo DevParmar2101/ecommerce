230dc2224d91155f81ffebd734574c72c923a802
Dev <devp2101@gmail.com>
2022-03-23 09:04:56 +0530

    new code is added in BlogController.php

--- frontend/controllers/BlogController.php
+++ frontend/controllers/BlogController.php
@@ -6,6 +6,7 @@ use common\models\Blog;
 use common\models\BlogSearch;
 use yii;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
 /**
  * Blog Controller
@@ -30,8 +31,25 @@ class BlogController extends Controller
         ]);
     }
 
-    public function actionBlogInner()
+    public function actionBlogInner($slug)
     {
-        return $this->render('blog-inner');
+
+        return $this->render('blog-inner',[
+            'model' => $this->findModel($slug)
+        ]);
+    }
+
+    /**
+     * @param $slug
+     * @return Blog|null
+     * @throws NotFoundHttpException
+     */
+    protected function findModel($slug)
+    {
+        if (($model = Blog::findOne($slug)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
     }
 }
